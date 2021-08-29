aab4d6955aff591828c05c03f60ed3892137b19a
clifford <cliffords2105@gmail.com>
2021-08-29 18:45:12 +0200

    The visitors sign in/out app was created and its database created as well.

--- root/visitor_register/models.py
+++ root/visitor_register/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitor(models.Model):
+    resident_student_number = models.CharField(max_length = 50)
+    resident_name = models.CharField(max_length = 50)
+    resident_surname = models.CharField(max_length = 50)
+    visitor_identity = models.CharField(max_length = 50)
+    visitor_name = models.CharField(max_length = 50)
+    visitor_surname = models.CharField(max_length = 50)
+    date_and_time_in = models.DateTimeField()
+    date_and_time_out = models.DateTimeField()
+
+    def __str__(self):
+        return self.resident_student_number
