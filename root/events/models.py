b6566daed5944b856fe7054b5e919abee0bc2e2f
clifford <cliffords2105@gmail.com>
2021-08-29 18:43:45 +0200

    The events app was created and its database created as well.

--- root/events/models.py
+++ root/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    event_name = models.CharField(max_length = 50)
+    event_date = models.DateField()
+    event_start_time = models.TimeField()
+    event_end_time = models.TimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.event_name
